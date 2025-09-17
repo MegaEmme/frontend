8633d277e2f9c127d9d0e1aac74a1fbaa84dbee5
MegaEmme <mikmusco@gmail.com>
2025-09-17 13:46:21 +0200

    feat: create show

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,8 +1,32 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GlobalContext } from "../contexts/GlobalContext";
+import FilmCard from "../components/FilmCard";
+
 const FilmPage = () => {
+    const { id } = useParams();
+    // Ricevo un singolo film, con le sue categorie, stato iniziale => oggetto vuoto ({})
+    const [film, setFilm] = useState({});
+    const { setIsLoading } = useContext(GlobalContext);
+
+    const fetchFilm = () => {
+        setIsLoading(true)
+        axios.get(`${import.meta.env.VITE_API_URL}/${id}`)
+            .then(response => {
+                const { data } = response;
+                setFilm(data)
+            })
+            .catch(error => console.log(error))
+            .then(() => { setIsLoading(false) })
+    };
+
+    useEffect(fetchFilm, []);
 
     return (
         <>
-            <h2>Ciao!</h2>
+            <h2>{film.title}</h2>
+            {film.id && <FilmCard film={film} isDetail={true} />}
         </>
     );
 };
