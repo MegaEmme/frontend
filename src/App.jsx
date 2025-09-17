6c1d8cd19745bad24c859625abc400fb2db37e3f
MegaEmme <mikmusco@gmail.com>
2025-09-17 12:16:05 +0200

    create routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import HomePage from "./pages/HomePage"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import HomePage from "./pages/HomePage";
+import FilmPage from "./pages/FilmPage";
+import GlobalProvider from "./contexts/GlobalContext";
+import DefaultLayout from "./layouts/DefaultLayout";
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
+      <GlobalProvider value={{ isLoading, setIsLoading }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/films/:id" element={<FilmPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalProvider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
