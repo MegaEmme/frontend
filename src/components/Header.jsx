015278c1d03701e1f27ccd16bbc7f8c0c4db80e4
MegaEmme <mikmusco@gmail.com>
2025-09-17 12:45:36 +0200

    feat: add styles, create filler div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
 
     return (
-        <nav className="navbar bg-body-tertiary mb-4">
-            <div className="container-fluid justify-content-start">
-                <Link className="navbar-brand" to="/">Cineteca di Mike</Link>
-            </div>
-        </nav>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4 shadow fixed-top">
+                <div className="container-fluid justify-content-start">
+                    <Link className="navbar-brand" to="/">Lista Film</Link>
+                </div>
+            </nav>
+            <div className="filler"></div>
+        </>
     );
 };
 
