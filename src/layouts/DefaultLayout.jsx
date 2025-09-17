4b01d36e12776e38c13621f7bf5dccafed189d04
MegaEmme <mikmusco@gmail.com>
2025-09-17 12:35:31 +0200

    feat: imported global Context

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,13 +2,13 @@ import { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Loader from "../components/Loader";
+import { GlobalContext } from "../contexts/GlobalContext";
 
 const DefaultLayout = () => {
 
     const { isLoading } = useContext(GlobalContext);
 
     return (
-
         <>
             <Header />
             <main className="container">
