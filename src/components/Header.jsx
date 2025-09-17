01ae99c7305e15458859d5e488f6b4cbdc84aa1f
MegaEmme <mikmusco@gmail.com>
2025-09-17 12:35:43 +0200

    feat: add  ;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
 
     return (
         <nav className="navbar bg-body-tertiary mb-4">
             <div className="container-fluid justify-content-start">
-                <Link className="navbar-brand" to="/">Cineteca</Link>
+                <Link className="navbar-brand" to="/">Cineteca di Mike</Link>
             </div>
         </nav>
-    )
+    );
+};
 
-}
-
-export default Header
+export default Header;
